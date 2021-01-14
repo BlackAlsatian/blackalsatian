c24278951c9609fd58b8fd08ae864f43c93961eb
likawoopitiki <blackalsatian@gmail.com>
2021-01-14 16:52:13 +0200

    Small change to config. Removed some console.logs.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -28,12 +28,12 @@ import './src/assets/css/styles.css'
 // }
 
 // Adjust if you need to add a delay before scrolling
-const transitionDelay = 500
+const transitionDelay = 300
 
 export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
     if (location.action === 'PUSH') {
         window.setTimeout(() => {
-            console.log('scroll to top')
+            // console.log('scroll to top')
             window.scrollTo({
                 top: 0,
                 behavior: 'smooth', // feel free to use or not
@@ -43,7 +43,7 @@ export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPo
         const savedPosition = getSavedScrollPosition(location) || [0, 0]
         const top = savedPosition[1]
         window.setTimeout(() => {
-            console.log('scroll to saved position')
+            // console.log('scroll to saved position')
             window.scrollTo({
                 top,
                 behavior: 'smooth',
