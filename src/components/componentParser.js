dee94ecefeef8f01b0daa9dc782a21c8f2421305
likawoopitiki <blackalsatian@gmail.com>
2021-04-19 17:56:07 +0200

    Testing without lazy-loader.

--- src/components/componentParser.js
+++ src/components/componentParser.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import LazyLoad from 'react-lazyload'
+// import LazyLoad from 'react-lazyload'
 import { randomID } from '../components/helpers'
 
 import HeroBlock from './template/blocks/heroBlock'
@@ -9,7 +9,7 @@ import TestimonialsBlock from '../components/template/blocks/testimonialsBlock'
 import FeaturedProjectsBlock from '../components/template/blocks/featuredProjects'
 import ServicesBlock from '../components/template/blocks/servicesBlock'
 import LatestPostsBlock from '../components/template/blocks/latestPostsBlock'
-import PlaceholderLoader from '../components/placeholderLoader'
+// import PlaceholderLoader from '../components/placeholderLoader'
 
 const ComponentParser = (props) => {
     const { blocks, featuredImage } = props
@@ -27,26 +27,10 @@ const ComponentParser = (props) => {
                         {__typename === 'WpBlackalsatianContentBlock' && (
                             <ContentBlock attributes={attributes} innerBlocks={innerBlocks} />
                         )}
-                        {__typename === 'WpBlackalsatianServicesBlock' && (
-                            <LazyLoad height='100%' offSet={150} once placeholder={<PlaceholderLoader />}>
-                                <ServicesBlock />
-                            </LazyLoad>
-                        )}
-                        {__typename === 'WpBlackalsatianLatestPostsBlock' && (
-                            <LazyLoad height='100%' offSet={150} once placeholder={<PlaceholderLoader />}>
-                                <LatestPostsBlock />
-                            </LazyLoad>
-                        )}
-                        {__typename === 'WpBlackalsatianFeaturedProjectsBlock' && (
-                            <LazyLoad height='100%' offSet={150} once placeholder={<PlaceholderLoader />}>
-                                <FeaturedProjectsBlock />
-                            </LazyLoad>
-                        )}
-                        {__typename === 'WpBlackalsatianTestimonialsBlock' && (
-                            <LazyLoad height='100%' offSet={150} once placeholder={<PlaceholderLoader />}>
-                                <TestimonialsBlock />
-                            </LazyLoad>
-                        )}
+                        {__typename === 'WpBlackalsatianServicesBlock' && <ServicesBlock />}
+                        {__typename === 'WpBlackalsatianLatestPostsBlock' && <LatestPostsBlock />}
+                        {__typename === 'WpBlackalsatianFeaturedProjectsBlock' && <FeaturedProjectsBlock />}
+                        {__typename === 'WpBlackalsatianTestimonialsBlock' && <TestimonialsBlock />}
                     </Fragment>
                 ))}
         </>
