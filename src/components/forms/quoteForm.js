2c463cd3a49c73850aedc990d98bfe7b8c1190c0
likawoopitiki <blackalsatian@gmail.com>
2021-04-25 03:04:40 +0200

    Created honeypot for spam. Tickbox for privacy policy. New API endpoint with additional fields for quote form.

--- src/components/forms/quoteForm.js
+++ src/components/forms/quoteForm.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { Link } from 'gatsby'
 import { Label, Input, Textarea, Box, Button, Spinner, Alert } from 'theme-ui'
 import { useState } from 'react'
 import { Formik } from 'formik'
@@ -21,23 +22,41 @@ const QuoteFormSchema = Yup.object({
         .max(80, "Wow, now that's a long email address! This field only accepts 80 characters.")
         .required('Oops! You missed this field.'),
     message: Yup.string().min(40, 'Was that all? At least 40 characters are required.').required('How may we assist?'),
+    privacy_policy: Yup.boolean()
+        .required()
+        .oneOf([true], "Without your permission, we won't be able to contact you. *sad face*"),
 })
+
+const sendGA = () => {
+    if (process.env.NODE_ENV !== 'production') {
+        console.log('Development')
+        console.log('GTag fired!')
+    } else {
+        // console.log('Production')
+        if (typeof window !== 'undefined') {
+            window.gtag('event', 'quote')
+        }
+    }
+}
 
 const QuoteForm = ({ buttonBackground, btnColor, formStyle, buttonName }) => {
     const [messageAlert, setMessageAlert] = useState(false)
+    let referrerUrl = typeof window !== 'undefined' && window.location.origin
+    let pathUrl = typeof window !== 'undefined' && window.location.pathname
     return (
         <Formik
             initialValues={{
                 name: '',
-                number: '',
+                lastname: '',
                 email: '',
+                number: '',
                 message: '',
-                status: 'Quote Request',
-                subscribe: false,
+                status: 'lead',
+                privacy_policy: false,
                 site: 'blackalsatian.co.za',
-                page: '',
-                source: '',
-                notes: '',
+                page: pathUrl,
+                traffic_source: referrerUrl,
+                tags: 'quote',
             }}
             validationSchema={QuoteFormSchema}
             onSubmit={(values, actions) => {
@@ -51,11 +70,12 @@ const QuoteForm = ({ buttonBackground, btnColor, formStyle, buttonName }) => {
                     data: JSON.stringify(values),
                 }).then(
                     (response) => {
+                        // console.log(response)
                         if (response.status === 201) {
                             setMessageAlert(true)
                             actions.resetForm()
                             actions.setSubmitting(false)
-                            typeof window !== 'undefined' && window.gtag('event', 'quote')
+                            sendGA()
                             setTimeout(() => {
                                 setMessageAlert(false)
                             }, 4000)
@@ -94,6 +114,37 @@ const QuoteForm = ({ buttonBackground, btnColor, formStyle, buttonName }) => {
                             {formik.errors.name}
                         </div>
                     ) : null}
+
+                    <Label
+                        htmlFor='lastname'
+                        sx={{
+                            opacity: 0,
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            height: 0,
+                            width: 0,
+                            zIndex: -1,
+                        }}
+                    >
+                        Lastname
+                    </Label>
+                    <Input
+                        id='lastname'
+                        name='lastname'
+                        type='text'
+                        sx={{
+                            opacity: 0,
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            height: 0,
+                            width: 0,
+                            zIndex: -1,
+                        }}
+                        autocomplete='off'
+                    />
+
                     <Label htmlFor='number'>Number</Label>
                     <Input
                         id='number'
@@ -119,6 +170,7 @@ const QuoteForm = ({ buttonBackground, btnColor, formStyle, buttonName }) => {
                             {formik.errors.number}
                         </div>
                     ) : null}
+
                     <Label htmlFor='email'>Email</Label>
                     <Input
                         id='email'
@@ -144,6 +196,7 @@ const QuoteForm = ({ buttonBackground, btnColor, formStyle, buttonName }) => {
                             {formik.errors.email}
                         </div>
                     ) : null}
+
                     <Label htmlFor='message'>Message</Label>
                     <Textarea
                         id='message'
@@ -169,6 +222,95 @@ const QuoteForm = ({ buttonBackground, btnColor, formStyle, buttonName }) => {
                             {formik.errors.message}
                         </div>
                     ) : null}
+
+                    <Label
+                        htmlFor='privacy_policy'
+                        mb={3}
+                        sx={{
+                            display: 'inline-flex',
+                            cursor: 'pointer',
+                            position: 'relative',
+                        }}
+                    >
+                        <span>
+                            <Input
+                                id='privacy_policy'
+                                type='checkbox'
+                                name='privacy_policy'
+                                sx={{
+                                    height: '25px',
+                                    width: '25px',
+                                    display: 'block',
+                                    appearance: 'none',
+                                    border: '1px solid white',
+                                    borderRadius: '4px',
+                                    outline: 'none',
+                                    transitionDuration: '0.3s',
+                                    backgroundColor: 'blue',
+                                    color: 'blue',
+                                    cursor: 'pointer',
+                                    '&:checked': {
+                                        border: '2px solid white',
+                                        backgroundColor: 'white',
+                                    },
+                                    '&:active': {
+                                        border: '2px solid white',
+                                    },
+                                }}
+                                {...formik.getFieldProps('privacy_policy')}
+                            />
+                        </span>
+                        <span
+                            sx={{
+                                color: 'white',
+                                padding: '0.25rem',
+                                fontSize: '0.75rem',
+                                '&::before': {
+                                    content: '"✓"',
+                                    color: 'blue',
+                                    display: 'block',
+                                    position: 'absolute',
+                                    left: '0.5rem',
+                                    top: '0.2rem',
+                                    fontWeight: 'bold',
+                                },
+                            }}
+                        ></span>
+                        <span sx={{ fontSize: '0.75rem', paddingLeft: '0.3rem' }}>
+                            We save the information you submit through this form for the sole purpose of contacting you
+                            regarding your query. You can also read our{' '}
+                            <Link
+                                to='/privacy-policy/'
+                                title='Black Alsatian Privacy Policy'
+                                sx={{ color: 'white', '&:hover': { color: 'offWhite', textDecoration: 'none' } }}
+                            >
+                                Privacy Policy
+                            </Link>{' '}
+                            and{' '}
+                            <Link
+                                to='/terms-of-use/'
+                                title='Black Alsatian Terms of Use'
+                                sx={{ color: 'white', '&:hover': { color: 'offWhite', textDecoration: 'none' } }}
+                            >
+                                Terms of Use
+                            </Link>{' '}
+                            for more info.
+                        </span>
+                    </Label>
+
+                    {formik.touched.privacy_policy && formik.errors.privacy_policy ? (
+                        <div
+                            sx={{
+                                color: 'yellow',
+                                fontSize: '0.8rem',
+                                fontWeight: 'bold',
+                                pb: 3,
+                            }}
+                        >
+                            {formik.errors.privacy_policy}
+                        </div>
+                    ) : null}
+
                     {messageAlert && (
                         <Alert
                             sx={{
