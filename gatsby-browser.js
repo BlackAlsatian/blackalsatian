2452cb8e772f3833a3efb0f9b0dc4a5b4f56763c
likawoopitiki <blackalsatian@gmail.com>
2021-05-26 16:13:01 +0200

    Updated NPM packages. Moved font css into browser and ssr files from layout file.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,10 @@
 import './src/assets/css/styles.css'
+import '@fontsource/montserrat/200.css'
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/400-italic.css'
+import '@fontsource/montserrat/700.css'
+import '@fontsource/montserrat/800.css'
+import '@fontsource/montserrat/900.css'
 import loadable from '@loadable/component'
 
 export const replaceHydrateFunction = () => {
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,3 +4,10 @@
 // export const wrapPageElement = ({ element, props }) => {
 //     return <Layout {...props}>{element}</Layout>
 // }
+import './src/assets/css/styles.css'
+import '@fontsource/montserrat/200.css'
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/400-italic.css'
+import '@fontsource/montserrat/700.css'
+import '@fontsource/montserrat/800.css'
+import '@fontsource/montserrat/900.css'
