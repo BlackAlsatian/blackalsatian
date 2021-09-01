de3ed23d857ddf4176e2f7809c7cc4c438c4648e
likawoopitiki <blackalsatian@gmail.com>
2021-09-01 16:49:31 +0200

    Edited preconnect config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,44 @@
-require(`dotenv`).config({
+require('dotenv').config({
     path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
     siteMetadata: {
-        title: `Black Alsatian`,
-        shortTitle: `blackalsatian`,
-        description: `Innovative custom web development, up-to-the-minute web design and bespoke marketing solutions that look amazing and get amazing results.`,
-        siteUrl: `https://www.blackalsatian.co.za`,
+        title: 'Black Alsatian',
+        shortTitle: 'blackalsatian',
+        description:
+            'Innovative custom web development, up-to-the-minute web design and bespoke marketing solutions that look amazing and get amazing results.',
+        siteUrl: 'https://www.blackalsatian.co.za',
         author: {
-            name: `Björn Potgieter`,
-            url: `https://bjorn.africa`,
-            twitter: `@bjornalism`,
+            name: 'Björn Potgieter',
+            url: 'https://bjorn.africa',
+            twitter: '@bjornalism',
         },
         organization: {
-            name: `Black Alsatian`,
-            alternateName: `Artisan Web Design Company by Passionate Web Developers`,
-            url: `https://www.blackalsatian.co.za`,
-            logo: `/images/blackalsatian-icon-112x112.png`,
-            pubLogo: `/images/blackalsatian-pub-logo.png`,
-            email: `info@blackalsatian.com`,
-            telephone: `+27 87 821 7172`,
+            name: 'Black Alsatian',
+            alternateName: 'Artisan Web Design Company by Passionate Web Developers',
+            url: 'https://www.blackalsatian.co.za',
+            logo: '/images/blackalsatian-icon-112x112.png',
+            pubLogo: '/images/blackalsatian-pub-logo.png',
+            email: 'info@blackalsatian.com',
+            telephone: '+27 87 821 7172',
         },
-        lang: `en`,
-        twitter: `@blackalsatian`,
+        lang: 'en',
+        twitter: '@blackalsatian',
         fbAppId: process.env.GATSBY_FBAPP_ID,
         socialUrls: {
-            twitter: `https://twitter.com/blackalsatian`,
-            facebook: `https://www.facebook.com/blackalsatian`,
-            instagram: `https://instagram.com/theblackalsatian`,
-            linkedin: `https://www.linkedin.com/company/blackalsatian`,
+            twitter: 'https://twitter.com/blackalsatian',
+            facebook: 'https://www.facebook.com/blackalsatian',
+            instagram: 'https://instagram.com/theblackalsatian',
+            linkedin: 'https://www.linkedin.com/company/blackalsatian',
         },
-        image: `/images/black-alsatian-dark-dog.png`,
-        pubLogo: `/images/blackalsatian-pub-logo.png`,
-        pubIconLogo: `/images/blackalsatian-icon-logo.png`,
+        image: '/images/black-alsatian-dark-dog.png',
+        pubLogo: '/images/blackalsatian-pub-logo.png',
+        pubIconLogo: '/images/blackalsatian-icon-logo.png',
         blog: {
-            title: `Blog`,
-            browserTitle: `A Web Design Company's Blog`,
-            intro: `Latest trends in the world of web development & digital marketing`,
+            title: 'Blog',
+            browserTitle: "A Web Design Company's Blog",
+            intro: 'Latest trends in the world of web development & digital marketing',
         },
     },
     flags: {
@@ -60,16 +61,16 @@ module.exports = {
         //  (Umbrella Issue (https://gatsby.dev/parallel-sourcing-feedback)) · Run all source plugins at the same time instead of serially. For sites with multiple source plugins, this can speedup sourcing and transforming considerably.
     },
     plugins: [
-        `gatsby-plugin-preact`,
+        'gatsby-plugin-preact',
         {
-            resolve: `gatsby-plugin-preconnect`,
+            resolve: 'gatsby-plugin-preconnect',
             options: {
-                domains: [`https://www.googletagmanager.com`, `https://www.google-analytics.com`],
+                domains: ['https://www.googletagmanager.com'],
             },
         },
-        `gatsby-plugin-react-helmet`,
+        'gatsby-plugin-react-helmet',
         {
-            resolve: `gatsby-plugin-google-tagmanager`,
+            resolve: 'gatsby-plugin-google-tagmanager',
             options: {
                 id: process.env.GATSBY_GTM_TRACKING_ID,
                 includeInDevelopment: false,
@@ -94,15 +95,15 @@ module.exports = {
                 enableWebVitalsTracking: true,
             },
         },
-        `gatsby-plugin-theme-ui`,
+        'gatsby-plugin-theme-ui',
         {
-            resolve: `gatsby-plugin-transition-link`,
+            resolve: 'gatsby-plugin-transition-link',
             options: {
-                layout: require.resolve(`./src/components/layout.js`),
+                layout: require.resolve('./src/components/layout.js'),
             },
         },
         {
-            resolve: `gatsby-source-wordpress`,
+            resolve: 'gatsby-source-wordpress',
             options: {
                 url: process.env.GATSBY_WPGRAPHQL_URL,
                 debug: {
@@ -122,7 +123,7 @@ module.exports = {
                     // previewRequestConcurrency: 2,
                 },
                 verbose: true,
-                excludeFieldNames: [`blocksJSON`],
+                excludeFieldNames: ['blocksJSON'],
                 develop: {
                     nodeUpdateInterval: 1000,
                 },
@@ -159,94 +160,94 @@ module.exports = {
                 },
             },
         },
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-purgecss`,
+        'gatsby-plugin-sass',
+        'gatsby-plugin-purgecss',
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: 'gatsby-source-filesystem',
             options: {
-                name: `images`,
+                name: 'images',
                 path: `${__dirname}/src/assets/images`,
             },
         },
-        `gatsby-plugin-image`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
+        'gatsby-plugin-image',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
         {
-            resolve: `gatsby-plugin-manifest`,
+            resolve: 'gatsby-plugin-manifest',
             options: {
-                name: `Black Alsatian`,
-                short_name: `Black Alsatian`,
-                description: `Handcrafted web applications and bespoke digital marketing solutions`,
-                start_url: `/`,
-                background_color: `#111827`,
-                theme_color: `#111827`,
-                display: `standalone`,
-                icon: `src/assets/images/blackalsatian-icon.png`,
+                name: 'Black Alsatian',
+                short_name: 'Black Alsatian',
+                description: 'Handcrafted web applications and bespoke digital marketing solutions',
+                start_url: '/',
+                background_color: '#111827',
+                theme_color: '#111827',
+                display: 'standalone',
+                icon: 'src/assets/images/blackalsatian-icon.png',
                 icons: [
                     {
-                        src: `/favicon/icon-16x16.png`,
-                        sizes: `16x16`,
-                        type: `image/png`,
+                        src: '/favicon/icon-16x16.png',
+                        sizes: '16x16',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-32x32.png`,
-                        sizes: `32x32`,
-                        type: `image/png`,
+                        src: '/favicon/icon-32x32.png',
+                        sizes: '32x32',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-48x48.png`,
-                        sizes: `48x48`,
-                        type: `image/png`,
+                        src: '/favicon/icon-48x48.png',
+                        sizes: '48x48',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-64x64.png`,
-                        sizes: `64x64`,
-                        type: `image/png`,
+                        src: '/favicon/icon-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-72x72.png`,
-                        sizes: `72x72`,
-                        type: `image/png`,
+                        src: '/favicon/icon-72x72.png',
+                        sizes: '72x72',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-96x96.png`,
-                        sizes: `96x96`,
-                        type: `image/png`,
-                        purpose: `any maskable`,
+                        src: '/favicon/icon-96x96.png',
+                        sizes: '96x96',
+                        type: 'image/png',
+                        purpose: 'any maskable',
                     },
                     {
-                        src: `/favicon/icon-144x144.png`,
-                        sizes: `144x144`,
-                        type: `image/png`,
+                        src: '/favicon/icon-144x144.png',
+                        sizes: '144x144',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-192x192.png`,
-                        sizes: `192x192`,
-                        type: `image/png`,
-                        purpose: `any maskable`,
+                        src: '/favicon/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'any maskable',
                     },
                     {
-                        src: `/favicon/icon-256x256.png`,
-                        sizes: `256x256`,
-                        type: `image/png`,
+                        src: '/favicon/icon-256x256.png',
+                        sizes: '256x256',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-384x384.png`,
-                        sizes: `384x384`,
-                        type: `image/png`,
+                        src: '/favicon/icon-384x384.png',
+                        sizes: '384x384',
+                        type: 'image/png',
                     },
                     {
-                        src: `/favicon/icon-512x512.png`,
-                        sizes: `512x512`,
-                        type: `image/png`,
-                        purpose: `any maskable`,
+                        src: '/favicon/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
                     },
                 ],
-                cache_busting_mode: `none`,
+                cache_busting_mode: 'none',
             },
         },
         {
-            resolve: `gatsby-plugin-advanced-sitemap`,
+            resolve: 'gatsby-plugin-advanced-sitemap',
             options: {
                 query: `
                 {
@@ -334,15 +335,15 @@ module.exports = {
                 output: '/sitemap.xml',
                 mapping: {
                     allWpPage: {
-                        sitemap: `pages`,
-                        prefix: `/`,
+                        sitemap: 'pages',
+                        prefix: '/',
                         // Custom Serializer
                         serializer: (edges) => {
                             return edges.map(({ node }) => {
                                 const PageFeaturedImagePath =
                                     node.featuredImage !== null
                                         ? node.featuredImage.node.localFile.publicURL
-                                        : `/images/black-alsatian-dark-dog.png`
+                                        : '/images/black-alsatian-dark-dog.png'
                                 return {
                                     node: {
                                         id: node.id,
@@ -355,14 +356,14 @@ module.exports = {
                         },
                     },
                     allWpPost: {
-                        sitemap: `blog`,
-                        prefix: `blog/`,
+                        sitemap: 'blog',
+                        prefix: 'blog/',
                         serializer: (edges) => {
                             return edges.map(({ node }) => {
                                 const PostFeaturedImagePath =
                                     node.featuredImage !== null
                                         ? node.featuredImage.node.localFile.publicURL
-                                        : `/images/black-alsatian-dark-dog.png`
+                                        : '/images/black-alsatian-dark-dog.png'
                                 return {
                                     node: {
                                         id: node.id,
@@ -375,14 +376,14 @@ module.exports = {
                         },
                     },
                     allWpService: {
-                        sitemap: `services`,
-                        prefix: `services/`,
+                        sitemap: 'services',
+                        prefix: 'services/',
                         serializer: (edges) => {
                             return edges.map(({ node }) => {
                                 const ServiceFeaturedImagePath =
                                     node.featuredImage !== null
                                         ? node.featuredImage.node.localFile.publicURL
-                                        : `/images/black-alsatian-dark-dog.png`
+                                        : '/images/black-alsatian-dark-dog.png'
                                 return {
                                     node: {
                                         id: node.id,
@@ -395,14 +396,14 @@ module.exports = {
                         },
                     },
                     allWpLander: {
-                        sitemap: `landers`,
-                        prefix: `x/`,
+                        sitemap: 'landers',
+                        prefix: 'x/',
                         serializer: (edges) => {
                             return edges.map(({ node }) => {
                                 const LanderFeaturedImagePath =
                                     node.featuredImage !== null
                                         ? node.featuredImage.node.localFile.publicURL
-                                        : `/images/black-alsatian-dark-dog.png`
+                                        : '/images/black-alsatian-dark-dog.png'
                                 return {
                                     node: {
                                         id: node.id,
@@ -415,14 +416,14 @@ module.exports = {
                         },
                     },
                     allWpPortfolio: {
-                        sitemap: `portfolio`,
-                        prefix: `portfolio/`,
+                        sitemap: 'portfolio',
+                        prefix: 'portfolio/',
                         serializer: (edges) => {
                             return edges.map(({ node }) => {
                                 const ProjectFeaturedImagePath =
                                     node.featuredImage !== null
                                         ? node.featuredImage.node.localFile.publicURL
-                                        : `/images/black-alsatian-dark-dog.png`
+                                        : '/images/black-alsatian-dark-dog.png'
                                 return {
                                     node: {
                                         id: node.id,
@@ -436,19 +437,19 @@ module.exports = {
                     },
                 },
                 exclude: [
-                    `/dev-404-page`,
-                    `/404`,
-                    `/404.html`,
-                    `/offline-plugin-app-shell-fallback`,
-                    `/the-web-design-company-with-a-difference`,
+                    '/dev-404-page',
+                    '/404',
+                    '/404.html',
+                    '/offline-plugin-app-shell-fallback',
+                    '/the-web-design-company-with-a-difference',
                 ],
                 createLinkInHead: true,
                 addUncaughtPages: true,
             },
         },
-        `gatsby-plugin-gatsby-cloud`,
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-remove-generator`,
+        'gatsby-plugin-gatsby-cloud',
+        'gatsby-plugin-offline',
+        'gatsby-plugin-remove-generator',
         // {
         //     resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
         //     options: {
